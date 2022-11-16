2ea1bc06e8d4161bda711048c01c646cd0a2916f
mdaslamalidu <thehometutor2022@gmail.com>
2022-11-16 19:12:01 +0600

    update data

--- index.js
+++ index.js
@@ -56,6 +56,23 @@ async function run() {
 
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
+
+      const query = {
+        appointmentDate: booking.appointmentDate,
+        treatmentName: booking.treatmentName,
+        email: booking.email,
+      };
+
+      const alreadyBooking = await bookingsCollection.find(query).toArray();
+
+      if (alreadyBooking.length) {
+        const message = `you have already booked on this date ${booking.appointmentDate}`;
+        return res.send({
+          acknowledged: false,
+          message,
+        });
+      }
+
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
     });
